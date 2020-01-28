4524aebc5a35a60c0aa1861e1dbbbc3138cc05fc
kentonhuang <huangkenton@gmail.com>
2020-01-27 22:19:42 -0800

    box score celery tasks

--- nbalive/backend/api/views.py
+++ nbalive/backend/api/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from api.models import Game, BoxScore
 from api.serializers import GameSerializer, BoxScoreSerializer
+from example.tasks import sel_test
 
 from rest_framework import generics, status
+from rest_framework.response import Response
+
+
+import json
+import time
+import re
 # Create your views here.
 
 class GameDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+
+    def get(self, request, pk, *args, **kwargs):
+        if Game.objects.filter(pk=self.kwargs.get('pk')).exists():
+            game = Game.objects.get(pk=self.kwargs.get('pk'))
+            serializer = GameSerializer(game)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
 
 class GameList(generics.ListCreateAPIView):
     queryset = Game.objects.all()
@@ -20,3 +34,18 @@ class BoxScoreList(generics.ListCreateAPIView):
 class BoxScoreDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = BoxScore.objects.all()
     serializer_class = BoxScoreSerializer
+
+    def get(self, request, pk, *args, **kwargs):
+        if BoxScore.objects.filter(pk=self.kwargs.get('pk')).exists():
+            game = BoxScore.objects.get(pk=self.kwargs.get('pk'))
+            serializer = BoxScoreSerializer(game)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            pkstr = str(pk)
+            keys = {
+                'id': pkstr,
+                'game': pkstr   
+            }
+        new_boxscore = BoxScore.objects.create(**keys)
+        serializer = BoxScoreSerializer(new_boxscore)
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- nbalive/backend/example/tasks.py
+++ nbalive/backend/example/tasks.py
@@ -1,4 +1,5 @@
 import time
+import json
 
 from celery import shared_task
 from celery.utils.log import get_task_logger
@@ -6,7 +7,7 @@ from celery.utils.log import get_task_logger
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
+from api.models import Game, BoxScore
 
 logger = get_task_logger(__name__)
 
@@ -15,7 +16,9 @@ def test_task():
   logger.info('test task ran')
 
 @shared_task
-def sel_test():
+def sel_test(gameid):
+
+  logger.info(gameid)
 
   options = webdriver.ChromeOptions()
   options.add_argument('--verbose')
@@ -25,44 +28,298 @@ def sel_test():
   capabilities = options.to_capabilities()
 
   driver = webdriver.Remote(
-      command_executor='http://172.17.0.1:4444/wd/hub',
+      command_executor='http://172.18.0.4:4444/wd/hub',
       desired_capabilities=capabilities,
   )
-  # driver.get('http://f3c24081.ngrok.io/')
 
-  # score1 = driver.find_element_by_class_name('score1')
-  # score2 = driver.find_element_by_class_name('score2')
+  left = 0
+  right = 1800
+  
+  while right <= 28800:
+    url = 'https://stats.nba.com/game/%s/?StartRange=%s&EndRange=%s&RangeType=2' %(gameid, left, right)
+    driver.get(url) 
+    time.sleep(8)
 
-  # i = 0;
+    
 
-  # while i < 5:
-  #   logger.info(score1.text)
-  #   logger.info(score2.text)
-  #   i += 1
-  #   time.sleep(2)
-  score1 = [];
-  driver.get('https://stats.nba.com/game/0021900002/?StartRange=7200&EndRange=9000&RangeType=2')
-  #driver.get('https://google.com')
-  time.sleep(5)
+    tables = driver.find_elements_by_class_name('nba-stat-table__overflow')
+    players1 = tables[0].find_elements_by_xpath('.//table/tbody/tr')
+    players2 = tables[1].find_elements_by_xpath('.//table/tbody/tr')
 
-  el = driver.find_element_by_tag_name('body')
-  source = el.get_attribute("outerHTML");
-  # score1 = driver.find_element_by_class_name('nba-stat-table')
-  logger.info(source)
-  #score2 = driver.find_element_by_class_name('score2')
+    minutes = int(right/600)
 
-  # i = 0;
+    away_box = {
+      'time': minutes,
+      'players': {}
+    }
+    home_box = {
+      'time': minutes,
+      'players': {}
+    }
 
-  # while i < 5:
-  #   logger.info(score1.text)
-  #   logger.info(score2.text)
-  #   i += 1
-  #   time.sleep(2)
+    for player in players1:
+      rows = player.find_elements_by_tag_name('td')
+      player = rows[0].text
+      away_box['players'][player] = {}
+      away_box['players'][player]['minutes'] = rows[1].text
+      away_box['players'][player]['FGM'] = rows[2].text
+      away_box['players'][player]['FGA'] = rows[3].text
+      away_box['players'][player]['3PM'] = rows[5].text
+      away_box['players'][player]['3PA'] = rows[6].text
+      away_box['players'][player]['FTM'] = rows[8].text
+      away_box['players'][player]['FTA'] = rows[9].text
+      away_box['players'][player]['OREB'] = rows[11].text
+      away_box['players'][player]['DREB'] = rows[12].text
+      away_box['players'][player]['REB'] = rows[13].text
+      away_box['players'][player]['AST'] = rows[14].text
+      away_box['players'][player]['TOV'] = rows[15].text
+      away_box['players'][player]['STL'] = rows[16].text
+      away_box['players'][player]['BLK'] = rows[17].text
+      away_box['players'][player]['PF'] = rows[18].text
+      away_box['players'][player]['PTS'] = rows[19].text
+      away_box['players'][player]['PM'] = rows[20].text
+    
+    for player in players2:
+      rows = player.find_elements_by_tag_name('td')
+      player = rows[0].text
+      home_box['players'][player] = {}
+      home_box['players'][player]['minutes'] = rows[1].text
+      home_box['players'][player]['FGM'] = rows[2].text
+      home_box['players'][player]['FGA'] = rows[3].text
+      home_box['players'][player]['3PM'] = rows[5].text
+      home_box['players'][player]['3PA'] = rows[6].text
+      home_box['players'][player]['FTM'] = rows[8].text
+      home_box['players'][player]['FTA'] = rows[9].text
+      home_box['players'][player]['OREB'] = rows[11].text
+      home_box['players'][player]['DREB'] = rows[12].text
+      home_box['players'][player]['REB'] = rows[13].text
+      home_box['players'][player]['AST'] = rows[14].text
+      home_box['players'][player]['TOV'] = rows[15].text
+      home_box['players'][player]['STL'] = rows[16].text
+      home_box['players'][player]['BLK'] = rows[17].text
+      home_box['players'][player]['PF'] = rows[18].text
+      home_box['players'][player]['PTS'] = rows[19].text
+      home_box['players'][player]['PM'] = rows[20].text
+
+    away_json = json.dumps(away_box)
+    home_json = json.dumps(home_box)
+
+    keys = {}
+    awaykey = 'a_' + str(minutes)
+    homekey = 'h_' + str(minutes)
+
+    keys[awaykey] = away_box
+    keys[homekey] = home_box
+
+    BoxScore.objects.filter(pk=gameid).update(**keys)
+
+    logger.info(away_json)
+    logger.info(home_json)
+
+    left += 1800
+    right += 1800
+    time.sleep(3)
 
   driver.quit()
   
+@shared_task
+def get_quarter_box(gameid):
 
+  logger.info(gameid)
 
+  options = webdriver.ChromeOptions()
+  options.add_argument('--verbose')
+  options.add_argument("--whitelisted-ips")
+  options.add_argument("--start-maximized")                                                                                                                                
 
+  capabilities = options.to_capabilities()
 
+  driver = webdriver.Remote(
+      command_executor='http://172.18.0.6:4444/wd/hub',
+      desired_capabilities=capabilities,
+  )
+
+  left = 0
+  right = 7200
+  
+  while right <= 28800:
+    url = 'https://stats.nba.com/game/%s/?RangeType=2&StartRange=%s&EndRange=%s' %(gameid, left, right)
+    driver.get(url) 
+    time.sleep(8)
+
+    tables = driver.find_elements_by_class_name('nba-stat-table__overflow')
+    players1 = tables[0].find_elements_by_xpath('.//table/tbody/tr')
+    players2 = tables[1].find_elements_by_xpath('.//table/tbody/tr')
+
+    quarter = int(right/7200)
+
+    away_box = {
+      'players': {}
+    }
+    home_box = {
+      'players': {}
+    }
+
+    for player in players1:
+      rows = player.find_elements_by_tag_name('td')
+      player = rows[0].text
+      away_box['players'][player] = {}
+      away_box['players'][player]['minutes'] = rows[1].text
+      away_box['players'][player]['FGM'] = rows[2].text
+      away_box['players'][player]['FGA'] = rows[3].text
+      away_box['players'][player]['3PM'] = rows[5].text
+      away_box['players'][player]['3PA'] = rows[6].text
+      away_box['players'][player]['FTM'] = rows[8].text
+      away_box['players'][player]['FTA'] = rows[9].text
+      away_box['players'][player]['OREB'] = rows[11].text
+      away_box['players'][player]['DREB'] = rows[12].text
+      away_box['players'][player]['REB'] = rows[13].text
+      away_box['players'][player]['AST'] = rows[14].text
+      away_box['players'][player]['TOV'] = rows[15].text
+      away_box['players'][player]['STL'] = rows[16].text
+      away_box['players'][player]['BLK'] = rows[17].text
+      away_box['players'][player]['PF'] = rows[18].text
+      away_box['players'][player]['PTS'] = rows[19].text
+      away_box['players'][player]['PM'] = rows[20].text
+    
+    for player in players2:
+      rows = player.find_elements_by_tag_name('td')
+      player = rows[0].text
+      home_box['players'][player] = {}
+      home_box['players'][player]['minutes'] = rows[1].text
+      home_box['players'][player]['FGM'] = rows[2].text
+      home_box['players'][player]['FGA'] = rows[3].text
+      home_box['players'][player]['3PM'] = rows[5].text
+      home_box['players'][player]['3PA'] = rows[6].text
+      home_box['players'][player]['FTM'] = rows[8].text
+      home_box['players'][player]['FTA'] = rows[9].text
+      home_box['players'][player]['OREB'] = rows[11].text
+      home_box['players'][player]['DREB'] = rows[12].text
+      home_box['players'][player]['REB'] = rows[13].text
+      home_box['players'][player]['AST'] = rows[14].text
+      home_box['players'][player]['TOV'] = rows[15].text
+      home_box['players'][player]['STL'] = rows[16].text
+      home_box['players'][player]['BLK'] = rows[17].text
+      home_box['players'][player]['PF'] = rows[18].text
+      home_box['players'][player]['PTS'] = rows[19].text
+      home_box['players'][player]['PM'] = rows[20].text
+
+    away_json = json.dumps(away_box)
+    home_json = json.dumps(home_box)
+
+    keys = {}
+    awaykey = 'a_q' + str(quarter)
+    homekey = 'h_q' + str(quarter)
+
+    keys[awaykey] = away_box
+    keys[homekey] = home_box
+
+    BoxScore.objects.filter(pk=gameid).update(**keys)
+
+    logger.info(away_json)
+    logger.info(home_json)
+
+    left += 7200
+    right += 7200
+    time.sleep(3)
+
+  driver.quit()
+
+@shared_task
+def get_game_box(gameid):
+
+  logger.info(gameid)
+
+  options = webdriver.ChromeOptions()
+  options.add_argument('--verbose')
+  options.add_argument("--whitelisted-ips")
+  options.add_argument("--start-maximized")                                                                                                                                
+
+  capabilities = options.to_capabilities()
+
+  driver = webdriver.Remote(
+      command_executor='http://172.18.0.6:4444/wd/hub',
+      desired_capabilities=capabilities,
+  )
+
+  left = 0
+  right = 28800
+  
+  url = 'https://stats.nba.com/game/%s/?RangeType=2&StartRange=%s&EndRange=%s' %(gameid, left, right)
+  driver.get(url) 
+  time.sleep(8)
+
+  tables = driver.find_elements_by_class_name('nba-stat-table__overflow')
+  players1 = tables[0].find_elements_by_xpath('.//table/tbody/tr')
+  players2 = tables[1].find_elements_by_xpath('.//table/tbody/tr')
+
+  away_box = {
+    'players': {}
+  }
+  home_box = {
+    'players': {}
+  }
+
+  for player in players1:
+    rows = player.find_elements_by_tag_name('td')
+    player = rows[0].text
+    away_box['players'][player] = {}
+    away_box['players'][player]['minutes'] = rows[1].text
+    away_box['players'][player]['FGM'] = rows[2].text
+    away_box['players'][player]['FGA'] = rows[3].text
+    away_box['players'][player]['3PM'] = rows[5].text
+    away_box['players'][player]['3PA'] = rows[6].text
+    away_box['players'][player]['FTM'] = rows[8].text
+    away_box['players'][player]['FTA'] = rows[9].text
+    away_box['players'][player]['OREB'] = rows[11].text
+    away_box['players'][player]['DREB'] = rows[12].text
+    away_box['players'][player]['REB'] = rows[13].text
+    away_box['players'][player]['AST'] = rows[14].text
+    away_box['players'][player]['TOV'] = rows[15].text
+    away_box['players'][player]['STL'] = rows[16].text
+    away_box['players'][player]['BLK'] = rows[17].text
+    away_box['players'][player]['PF'] = rows[18].text
+    away_box['players'][player]['PTS'] = rows[19].text
+    away_box['players'][player]['PM'] = rows[20].text
+  
+  for player in players2:
+    rows = player.find_elements_by_tag_name('td')
+    player = rows[0].text
+    home_box['players'][player] = {}
+    home_box['players'][player]['minutes'] = rows[1].text
+    home_box['players'][player]['FGM'] = rows[2].text
+    home_box['players'][player]['FGA'] = rows[3].text
+    home_box['players'][player]['3PM'] = rows[5].text
+    home_box['players'][player]['3PA'] = rows[6].text
+    home_box['players'][player]['FTM'] = rows[8].text
+    home_box['players'][player]['FTA'] = rows[9].text
+    home_box['players'][player]['OREB'] = rows[11].text
+    home_box['players'][player]['DREB'] = rows[12].text
+    home_box['players'][player]['REB'] = rows[13].text
+    home_box['players'][player]['AST'] = rows[14].text
+    home_box['players'][player]['TOV'] = rows[15].text
+    home_box['players'][player]['STL'] = rows[16].text
+    home_box['players'][player]['BLK'] = rows[17].text
+    home_box['players'][player]['PF'] = rows[18].text
+    home_box['players'][player]['PTS'] = rows[19].text
+    home_box['players'][player]['PM'] = rows[20].text
+
+  away_json = json.dumps(away_box)
+  home_json = json.dumps(home_box)
+
+  keys = {}
+  awaykey = 'away'
+  homekey = 'home'
+
+  keys[awaykey] = away_box
+  keys[homekey] = home_box
+
+  BoxScore.objects.filter(pk=gameid).update(**keys)
+
+  logger.info(away_json)
+  logger.info(home_json)
+
+  time.sleep(3)
+
+  driver.quit()
 
